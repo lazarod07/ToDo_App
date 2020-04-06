e79782f97d524fca91011b2833c2f5b8cef1e81c
augustosalazar <augustosalazar@uninorte.edu.co>
2020-04-06 02:34:39 -0500

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:f_202010_todo_class/model/todo.dart';
+import 'package:flutter/material.dart';
+
+class HomePageTodo extends StatefulWidget {
+  @override
+  _HomePageTodoState createState() => _HomePageTodoState();
+}
+
+class _HomePageTodoState extends State<HomePageTodo> {
+  List<Todo> todos = new List<Todo>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo App"),
+      ),
+      body: _list(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _addTodo, tooltip: 'Add task', child: new Icon(Icons.add)),
+    );
+  }
+
+  Widget _list(){
+      return ListView.builder(
+      itemCount: todos.length,
+      itemBuilder: (context, posicion) {
+        var element = todos[posicion];
+        return _item(element, posicion);
+      },
+    );
+  }
+
+  Widget _item(Todo element, int posicion){
+    return Text('$posicion');
+  }
+
+  void _addTodo(){
+    setState(() {
+      todos.add(new Todo(title:"itemT", body: "itemB", completed: 0 ));
+    });
+  }
+}
